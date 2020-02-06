66c06cfd726520c12d4d4bad15e430c70175c90f
安頔 <and17@mails.tsinghua.edu.cn>
2020-02-06 15:16:25 +0800

    Food-Buying.cpp

--- codeforces/Round#617(div3)/Food-Buying.cpp
+++ codeforces/Round#617(div3)/Food-Buying.cpp
@@ -5,3 +5,20 @@
 #include <cstring>
 
 using namespace std;
+
+int T, n;
+
+int getAns(int n){
+	if(n < 10)return n;
+	int tmp = (int)(n/10);
+	return tmp*10+getAns(n-tmp*9);
+}
+
+int main(){
+	scanf("%d", &T);
+	while(T--){
+		scanf("%d", &n);
+		int ans = getAns(n);
+		printf("%d\n", ans);
+	}return 0;
+}
