7cb390a6226c2da5a5608703cf444c76055ec85f
安頔 <and17@mails.tsinghua.edu.cn>
2020-02-08 13:14:01 +0800

    Coffee-Varieties.cpp

--- codeforces/Round#616(div2)/src/Coffee-Varieties.cpp
+++ codeforces/Round#616(div2)/src/Coffee-Varieties.cpp
@@ -12,9 +12,11 @@ using namespace std;
 
 const int maxn = 2010;
 
-int n, k;
+int n, k, blo;
 bool vis[maxn];
+int nxt[maxn];
 char ch[maxn];
+int cnt = 0;
 
 void getPrint(int type, int x){
 	if(type == 0)printf("R\n");
@@ -23,36 +25,47 @@ void getPrint(int type, int x){
 	fflush(stdout);
 }
 
+void dfs(int bloNum){
+	int L = bloNum*k+1;
+	int R = (bloNum+1)*k;
+	for(int i = L; i <= R; ++i){
+		if(vis[i])continue;
+		getPrint(1, i);
+		scanf("%s", ch);
+		if(ch[0] == 'Y')vis[i] = true;
+	}
+	if(nxt[bloNum] < blo){
+		dfs(nxt[bloNum]);
+		nxt[bloNum]++;
+		++cnt;
+	}return;
+}
+
 int main(){
 	scanf("%d%d", &n, &k);
 	for(int i = 1; i <= n; ++i)vis[i] = false;
-	if(k == 1){
-		for(int i = 1; i <= n; ++i){
-			if(vis[i])continue;
-			for(int j = i+1; j <= n; ++j){
-				if(vis[j])continue;
-				getPrint(1, i);
-				scanf("%s", ch);
-				getPrint(1, j);
-				scanf("%s", ch);
-				if(ch[0] == 'Y')vis[j] = true;
-			}
-		}
-		int cnt = 0;
-		for(int i = 1; i <= n; ++i)if(!vis[i])++cnt;
-		getPrint(2, cnt);
-		return 0;
-	}
+	if(k > 1)k >>= 1;
 	for(int i = 1; i <= n; ++i){
 		getPrint(1, i);
 		scanf("%s", ch);
 		if(ch[0] == 'Y')vis[i] = true;
 	}
-	int blo = n/k;
+	blo = n/k;
 	for(int i = 0; i < blo; ++i){
-		int L = i*blo+1;
-		int R = (i+1)*blo;
-		int mid = L+(k>>1);
+		nxt[i] = i+2;
 	}
+	int lim = blo*(blo-1)/2-(blo-1);
+	while(cnt < lim){
+		getPrint(0, 0);
+		for(int i = 0; i < blo; ++i){
+			if(nxt[i] < blo){
+				dfs(i);
+				break;
+			}
+		}
+	}
+	cnt = 0;
+	for(int i = 1; i <= n; ++i)if(!vis[i])++cnt;
+	getPrint(2, cnt);
 	return 0;
 }
