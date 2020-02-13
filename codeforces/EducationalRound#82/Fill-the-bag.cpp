a0665e0e37b7491e0ae69a6c78ee0692f699eb79
安頔 <and17@mails.tsinghua.edu.cn>
2020-02-13 20:49:45 +0800

    EduRound#82 D

--- codeforces/EducationalRound#82/Fill-the-bag.cpp
+++ codeforces/EducationalRound#82/Fill-the-bag.cpp
@@ -12,40 +12,53 @@ typedef long long LL;
 
 const int maxm = 100010;
 
-priority_queue<int>Q;
+priority_queue<LL>Q;
 
 int T;
 int m;
-LL n, cnt;
+LL n;
 int a[maxm];
+
+int getCount(LL tmp){
+	for(int i = 0; i <= 60; ++i){
+		if((tmp >> i)&1LL)return i;
+	}return 0;
+}
 
 int main(){
 	scanf("%d", &T);
 	while(T--){
 		scanf("%lld%d", &n, &m);
-		cnt = 0;
+		while(!Q.empty())Q.pop();
 		for(int i = 1; i <= m; ++i){
 			scanf("%d", &a[i]);
-			cnt += a[i];
+			Q.push(-a[i]);
 		}
-		if(cnt < n)printf("-1\n");
-		else{
-			while(!Q.empty())Q.pop();
-			for(int i = 1; i <= m; ++i)Q.push(a[i]);
-			cnt = cnt-n;
-			int ans = 0;
-			while(!Q.empty() && cnt > 0){
-				int cur = Q.top(); Q.pop();
-				if(cur == 1)continue;
-				cur >>= 1;
-				if(cnt-cur >= 0){
-					cnt -= cur;
-					Q.push(cur);
-					++ans;
+		int ans = 0;
+		int cur = 0;
+		bool flag = true;
+		for(int i = 0; i <= 60; ++i){
+			if((n>>i)&1LL){
+				if(i < cur)continue;
+				if(Q.empty()){
+					flag = false;
+					break;
 				}
+				LL tmp = Q.top(); Q.pop();
+				tmp = -tmp;
+				int now = getCount(tmp);
+				ans += now-i;
+				cur = now;
 			}
-			if(cnt == 0)printf("%d\n", ans);
-			else printf("-1\n");
+			int cnt = 0;
+			while(!Q.empty() && Q.top() == -(1LL<<i)){
+				++cnt;
+				Q.pop();
+			}
+			cnt >>= 1;
+			while(cnt--)Q.push(-(1LL<<(i+1)));
 		}
+		if(!flag)printf("-1\n");
+		else printf("%d\n", ans);
 	}return 0;
 }
